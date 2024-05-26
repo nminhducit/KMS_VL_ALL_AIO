caefd92a05284cc8fc09857fcfcf6c0c373ee68a
Nguyễn Minh Đức <95899532+nminhducit@users.noreply.github.com>
2024-05-26 08:00:19 +0700

    Create KMS.ps1

---
+++ KMS.ps1
@@ -0,0 +1,20 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/nminhducit/KMS_VL_ALL_AIO/IMP/KMS.cmd'
+
+$rand = Get-Random -Maximum 1000
+$FilePath = "$env:TEMP\KMS_$rand.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
